3300e2c17540045797f32a2d623198f48b62fdaf
fjhdream <fjh_dream@163.com>
2020-05-03 19:00:22 +0800

    第一次对按键进行第一次监听

---
+++ src/main/java/com/fjhdream/tank/T.java
@@ -0,0 +1,17 @@
+package com.fjhdream.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame frame = new TankFrame();
+
+        while (true) {
+            Thread.sleep(500);
+            frame.repaint();
+        }
+    }
+}
