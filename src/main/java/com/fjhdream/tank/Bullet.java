de4e0dfe1d2b83fa84c522af73cb77e21f5156e5
fjhdream <fjh_dream@163.com>
2020-05-04 20:28:13 +0800

    双缓存解决画面闪烁问题

--- src/main/java/com/fjhdream/tank/Bullet.java
+++ src/main/java/com/fjhdream/tank/Bullet.java
@@ -2,43 +2,35 @@ package com.fjhdream.tank;
 
 import java.awt.*;
 
-public class Tank {
+
+
+public class Bullet {
+    private static final int SPEED = 10;
+    private static final int WIDTH = 30;
+    private static final int HEIGHT = 30;
     private int x, y;
     private Dir dir;
-    private static final int SPEED = 5;
 
-    private boolean moving = false;
-
-    public Tank(int x, int y, Dir dir) {
-        super();
+    public Bullet(int x, int y, Dir dir) {
         this.x = x;
         this.y = y;
         this.dir = dir;
     }
 
-    public Dir getDir() {
-        return dir;
-    }
-
-    public void setMoving(boolean moving) {
-        this.moving = moving;
-    }
-
-
-    public void setDir(Dir dir) {
-        this.dir = dir;
-    }
-
     public void paint(Graphics g) {
+
         Color c = g.getColor();
-        g.setColor(Color.YELLOW);
-        g.fillRect(x, y, 50, 50);
+        g.setColor(Color.RED);
+        g.fillOval(x,y,WIDTH, HEIGHT);
         g.setColor(c);
+
         move();
+
+
     }
 
     private void move() {
-        if (!moving) return;
+
         switch (dir) {
             case UP:
                 y -= SPEED;
@@ -56,4 +48,5 @@ public class Tank {
                 break;
         }
     }
+
 }
--- src/main/java/com/fjhdream/tank/TankFrame.java
+++ src/main/java/com/fjhdream/tank/TankFrame.java
@@ -11,9 +11,12 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
    Tank myTank = new Tank(200,200, Dir.DOWN);
+   Bullet bullet = new Bullet(200,200,Dir.LEFT);
+
+   static  final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
     public  TankFrame() {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
@@ -104,11 +107,29 @@ public class TankFrame extends Frame {
 
     }
 
+    //双缓冲防止闪烁
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_WIDTH);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
+
 
     @Override
     public void paint(Graphics g) {
 
         myTank.paint(g);
+        bullet.paint(g);
 
     }
 
